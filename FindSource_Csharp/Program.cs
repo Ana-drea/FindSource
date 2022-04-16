ab7e70f035f773a967045d39853ef53e3721822e
andrea <huiwen_zhou@hotmail.com>
2022-04-16 13:56:57 +0800

    upload source code

---
+++ FindSource_Csharp/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> enulist = new List<string>();
+            System.Console.WriteLine("Please enter the path of the enu file list:");
+            string EnuListPath = System.Console.ReadLine();
+
+            if (File.Exists(EnuListPath))
+            {
+                StreamReader sr = new StreamReader(EnuListPath);
+                String s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    enulist.Add(s);
+                }
+                String language = null;
+
+
+                System.Console.WriteLine("Type in a number to choose a target language:");
+                System.Console.WriteLine("1: deu");
+                System.Console.WriteLine("2. esp");
+                System.Console.WriteLine("3. fra");
+                System.Console.WriteLine("4. ita");
+                System.Console.WriteLine("5. jpn");
+                String languagecode = System.Console.ReadLine();
+                switch (languagecode)
+                {
+                    case "1":
+                        language = "\\de\\";
+                        break;
+                    case "2":
+                        language = "\\es\\";
+                        break;
+                    case "3":
+                        language = "\\fr\\";
+                        break;
+                    case "4":
+                        language = "\\it\\";
+                        break;
+                    case "5":
+                        language = "\\ja\\";
+                        break;
+                    default:
+                        System.Console.WriteLine("Wrong input, end the process");
+                        System.Environment.Exit(1);
+                        break;
+                }
+
+                System.Console.WriteLine("Type in '1' to print out and check your target folder path");
+                System.Console.WriteLine("Type in '2' to start copying to target folder");
+                String nextstep = System.Console.ReadLine();
+                switch (nextstep)
+                {
+                    case "1":
+                        ShowTargetLanFolder(enulist, language);
+                        break;
+                    case "2":
+                        CopyToTargetLanFolder(enulist, language);
+                        break;
+                    default:
+                        System.Console.WriteLine("Wrong input, end the process");
+                        System.Environment.Exit(1);
+                        break;
+                }
+
+            }
+            else
+            {
+                System.Console.WriteLine("Invalid path. Please check your input.");
+            }
+        }
+
+
+        public static void CopyToTargetLanFolder(List<string> enulist, String language)
+        {
+            for (int i = 0; i < enulist.Count; i++)
+            {
+                // 
+                string EnuFilePath = enulist[i];
+                string FileName = Path.GetFileName(EnuFilePath);
+                // 
+                string TargetFolder = Path.GetDirectoryName(EnuFilePath).Replace("\\en\\", language);
+
+                // 
+                string TargetFilePath = Path.Combine(TargetFolder, FileName + "_enu");
+                copyFileContent(EnuFilePath, TargetFilePath);
+            }
+        }
+        public static void ShowTargetLanFolder(List<string> enulist, String language)
+        {
+            for (int i = 0; i < enulist.Count; i++)
+            {
+                // 
+                string EnuFilePath = enulist[i];
+
+                // 
+                string TargetFolder = Path.GetDirectoryName(EnuFilePath).Replace("\\en\\", language);
+
+                // 
+                System.Console.WriteLine(TargetFolder);
+            }
+        }
+
+        public static void copyFileContent(string EnuFilePath, string TargetFilePath)
+        {
+            StreamReader sr = new StreamReader(EnuFilePath);
+            StreamWriter sw = new StreamWriter(TargetFilePath);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                sw.WriteLine(line);
+            }
+            sr.Close();
+            sw.Close();
+        }
+
+    }
+}
---
+++ FindSource_Java/findSource.java
@@ -0,0 +1,105 @@
+package moravia;
+
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class findSource {
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> enulist = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the path of the enu file list:");
+        String enulistpath = sc.nextLine();
+        File f = new File(enulistpath);
+        if (f.isFile()) {
+            BufferedReader bf1 = new BufferedReader(new FileReader(f));
+            String s;
+            while ((s = bf1.readLine()) != null) {
+                enulist.add(s);
+            }
+            String language = null;
+
+            System.out.println("Type in a number to choose a target language:");
+            System.out.println("1: deu");
+            System.out.println("2. esp");
+            System.out.println("3. fra");
+            System.out.println("4. ita");
+            System.out.println("5. jpn");
+            String languagecode = sc.nextLine();
+            switch (languagecode) {
+                case "1":
+                    language = "\\de\\";
+                    break;
+                case "2":
+                    language = "\\es\\";
+                    break;
+                case "3":
+                    language = "\\fr\\";
+                    break;
+                case "4":
+                    language = "\\it\\";
+                    break;
+                case "5":
+                    language = "\\ja\\";
+                    break;
+                default:
+                    System.out.println("Wrong input, end the process");
+                    System.exit(1);
+            }
+            System.out.println("Type in '1' to print out target folder path");
+            System.out.println("Type in '2' to start copying to target folder");
+            String nextstep = sc.nextLine();
+            switch (nextstep) {
+                case "1":
+                    showTargetLanFolder(enulist, language);
+                    break;
+                case "2":
+                    copyToTargetLanFolder(enulist, language);
+                    break;
+                default:
+                    System.out.println("Wrong input, end the process");
+                    System.exit(1);
+                    break;
+            }
+
+
+            bf1.close();
+        } else {
+            System.out.println("Invalid path. Please check your input.");
+        }
+
+    }
+
+    public static void copyToTargetLanFolder(ArrayList<String> enulist, String language) throws IOException {
+        for (int i = 0; i < enulist.size(); i++) {
+            File enufile = new File(enulist.get(i));
+            String filename = enufile.getName();
+            String targetfolder = enufile.getParent().replace("\\en\\", language);
+            File targetFile = new File(targetfolder, filename + "_enu");
+            copyFileContent(enufile, targetFile);
+        }
+    }
+
+    public static void showTargetLanFolder(ArrayList<String> enulist, String language) throws IOException {
+        for (int i = 0; i < enulist.size(); i++) {
+            File enufile = new File(enulist.get(i));
+            String filename = enufile.getName();
+            String targetfolder = enufile.getParent().replace("\\en\\", language);
+            System.out.println(targetfolder);
+        }
+    }
+
+
+    public static void copyFileContent(File source, File target) throws IOException {
+        byte[] bys = new byte[1024];
+        int len;
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(source));
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(target));
+        while ((len = bis.read(bys)) != -1) {
+            bos.write(bys, 0, len);
+        }
+        bis.close();
+        bos.close();
+    }
+}
